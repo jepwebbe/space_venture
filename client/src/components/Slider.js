6fa7f4b56d69d66666f33441bae44b40fc7963e3
jepwebbe <94107611+jepwebbe@users.noreply.github.com>
2022-08-24 08:40:19 +0200

    slider timeout corrected, and maps full width

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useRef, useEffect } from "react";
 import Banner1 from "../assets/images/banner1-fw.jpg";
 import Banner2 from "../assets/images/banner2-fw.jpg";
 import Banner3 from "../assets/images/banner3-fw.jpg";
@@ -8,25 +9,50 @@ const images = [Banner1, Banner2, Banner3];
 const delay = 5000;
 // https://tinloof.com/blog/how-to-build-an-auto-play-slideshow-with-react
 export default function Slideshow() {
-    const [index, setIndex] = React.useState(0);
+    /*     const [index, setIndex] = React.useState(0);
+    
+        React.useEffect(() => {
+            setTimeout(
+                () =>
+                    setIndex((prevIndex) =>
+                        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+                    ),
+                delay
+            );
+    
+            return () => { };
+        }, [index]); */
+    // Daniels code below, as code above acted out bc of setTimeout apparently
+    const slideToBeActiv = [0, 1, 2];
+    const [index, setIndex] = useState(0);
+    const timeoutRef = useRef(null);
 
-    React.useEffect(() => {
-        setTimeout(
+    function resetTimeout() {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+    }
+
+    useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
             () =>
                 setIndex((prevIndex) =>
-                    prevIndex === images.length - 1 ? 0 : prevIndex + 1
+                    prevIndex === slideToBeActiv.length - 1 ? 0 : prevIndex + 1
                 ),
             delay
         );
 
-        return () => { };
+        return () => {
+            resetTimeout();
+        };
     }, [index]);
 
 
     return (
         <section className="slideshow">
             <div className="slideshowSlider"
-                style={{ transform: `translateY(${-index * 550}px)` }}>
+                style={{ transform: `translateY(${-index * 33}%)` }}>
                 {images.map((image, index) => (
                     <img className="slide" key={index} src={image} />
                 ))}
--- client/src/pages/Kontakt.js
+++ client/src/pages/Kontakt.js
@@ -4,7 +4,7 @@ import "./Kontakt.scss"
 function Kontakt() {
     return (
         <section className="Kontakt">
-            <div className="MapWrap"><iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=12.608892917633057%2C55.688562613985%2C12.623054981231691%2C55.693938692085794&amp;layer=mapnik"></iframe></div>
+            <div className="MapWrap"><iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=12.608892917633057%2C55.688562613985%2C12.623054981231691%2C55.693938692085794&amp;layer=mapnik"></iframe></div>
             <div className="KontaktWrap"><h1>Kontakt</h1><p>Skulle du have et spørgsmål eller to, så skriv endelig til os og vi vil kontakte dig hurtigst muligt</p></div>
             <KontaktForm />
         </section>
